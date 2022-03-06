0b65051de82c7dc3b6229c379518a55d5f59b8d4
BianXuerui <407955981@qq.com>
2022-03-06 16:31:54 +0800

    docs: 新增README文档

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,3 +8,15 @@ module.exports = {
     ],
   },
 };
+
+/**
+ * https://segmentfault.com/a/1190000017790694
+ * upd: 更新
+ * feat: 新增
+ * fix: 修复
+ * docs: 文档
+ * style: 样式
+ * refactor: 重构代码
+ * test: 单元测试
+ * chore: 构建过程或辅助工具的变动
+ */
